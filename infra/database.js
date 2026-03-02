06c2e4129cdae7816d685a348d2960108d52701a
wagner <wagncarv@outlook.com>
2026-03-02 14:52:43 -0300

    add `database.js` and `.env` files

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    password: "postgres",
-    database: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
   });
 
   await client.connect();
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,6 @@
 import database from "../../../../infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows[0].sum);
   response.status(200).json({ ok: true });
 }
 
