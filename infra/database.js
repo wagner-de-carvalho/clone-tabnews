5b8d2453aeff32bd0a9aeb10b849bef17af5595f
wagner <wagncarv@outlook.com>
2026-03-02 11:20:11 -0300

    feature: criar módulo "database.js"

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "postgres",
+    password: "postgres",
+    database: "postgres",
+  });
+
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
