ef6deed079baf2f1106395fb2de965a8af6639fe
wagner <wagncarv@outlook.com>
2026-03-07 21:32:52 -0300

    fix(database): ssl

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,9 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   try {
